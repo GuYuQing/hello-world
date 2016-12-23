466dd87eff44095b04f3d537e87b77c2d6ba0d91
GuYuQing <loveflying000@sina.com>
2016-12-23 17:53:34 +0800

    commit modify

--- nodeJs_test.js
+++ nodeJs_test.js
@@ -3,8 +3,10 @@ var http = require("http"); // http 模块
 http.createServer(function(req, res) {  
   res.writeHead( 200 , {"Content-Type":"text/html"});  
   res.write("<h1>Node.js</h1>");  
-  res.write("<p>Hello World</p>");  
+  res.write("<p>Hello World</p>");  //这里有点画面
   res.end("<p>beyondweb.cn</p>");  
 }).listen(3000); // 监听端口3000  
   
 console.log("HTTP server is listening at port 3000."); //在CMD中打印日志  
+
+//这里有点画面
